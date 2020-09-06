01916753375dabda68ca19dcedc8d9aa2271426f
xs <1744110814@qq.com>
2020-09-06 13:26:16 +0800

    redis 练习

--- application/test2/controller/Demo4.php
+++ application/test2/controller/Demo4.php
@@ -4,7 +4,18 @@
 namespace app\test2\controller;
 
 
+use think\facade\Cache;
+
 class Demo4
 {
+    /**
+     * 用于测试 TP 连接Redis
+     */
+
+
+    public function fun1()
+    {
+        Cache::set('name', 'xs', 3600);
+    }
 
 }
